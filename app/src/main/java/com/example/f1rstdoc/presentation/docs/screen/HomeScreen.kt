df729a5564540e0cc14eb2f96fc2fe3eb8b1aa18
wesley oliveira <wlwwesley9@gmail.com>
2025-10-13 21:54:20 -0300

    feat(VerticalGrid): apply verticalGrid in lazyColomn in homeScreen

--- app/src/main/java/com/example/f1rstdoc/presentation/docs/screen/HomeScreen.kt
+++ app/src/main/java/com/example/f1rstdoc/presentation/docs/screen/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.f1rstdoc.presentation.docs.screen
 
 import androidx.compose.animation.core.copy
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -8,6 +9,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -187,8 +191,11 @@ fun HomeContent(viewModel: DocsViewModel) {
             val getDocsstate = collectAsGetDocsState.value
                 when (getDocsstate) {
                     is GetDocsState.ListDocs -> {
-                        LazyColumn(
+                        LazyVerticalGrid(
                             modifier = Modifier.fillMaxSize(),
+                            columns = GridCells.Fixed(2),
+                            horizontalArrangement = Arrangement.spacedBy(12.dp),
+                            verticalArrangement = Arrangement.spacedBy(12.dp),
                             contentPadding = PaddingValues(16.dp) // Adiciona espaçamento nas bordas da lista
                         ) {
                             items(
